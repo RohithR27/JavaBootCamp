3d51986c5ca11a9f13d8c9c2f806e080b9cc1952
Rohith R <imrohi27@gmail.com>
2022-09-24 20:00:23 +0530

    First Commit

---
+++ sumofnaturalnum.java
@@ -0,0 +1,10 @@
+public class sumofnaturalnum {
+    public static void main(String[] args) {
+        int n = 5;
+        int sum = 0;
+        for(int i = 1; i <=n;i++){
+            sum = sum + i;
+        }
+        System.out.println("The Sum of natural numbers in range 1 to 50 is : "+sum);
+    }
+}
---
+++ swapnumbers.java
@@ -0,0 +1,14 @@
+public class swapnumbers{
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 23;
+        System.out.println("Before Swaping : \n a = "+a+" \n b = "+b);
+        //swaping of 2 numbers
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After Swaping : \n a = "+a+" \n b = "+b);
+        
+
+    }
+}
